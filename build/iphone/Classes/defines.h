b2812d375040854de63ec1d2ffe43e8014f17943
nihaopaul <nihaopaul@gmail.com>
2012-07-18 00:43:04 +0800

    cleaned up

--- build/iphone/Classes/defines.h
+++
@@ -1,8 +0,0 @@
-// Warning: this is generated file. Do not modify!
-
-#define TI_VERSION 2.1.0.GA
-#define USE_TI_ANALYTICS
-#define USE_TI_NETWORK
-#define USE_TI_PLATFORM
-#define USE_TI_UI
-#define USE_TI_API
