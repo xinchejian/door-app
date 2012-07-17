9a613cf3b60b426b830a9b42f066f59f89bc0837
nihaopaul <nihaopaul@gmail.com>
2012-07-17 23:12:35 +0800

    first save

---
+++ Resources/ui/common/FirstView.js
@@ -0,0 +1,107 @@
+Ti.App.code = '';
+function FirstView() {
+	//create object instance, a parasitic subclass of Observable
+	var self = Ti.UI.createView();
+
+	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
+	var label = Ti.UI.createLabel({
+		color:'#000000',
+		text:L('notice'),
+		height:'auto',
+		width:'auto',
+		bottom: '10%'
+	});
+	self.add(label);
+	//Add behavior for UI
+
+
+	var MR = require('ui/common/machineroom');
+	var PassPort = require('ui/common/passport');
+
+
+	
+	
+	var tl = 0;
+	var tt = 0;
+	for (var i = 1; i<= 12; i++) {
+		
+		if (i == 4 || i == 7 || i == 10) {
+			tl = 1;
+			tt++;
+		} else {
+			tl++;
+		}
+		
+		/* i learnt this trick from Lio! */
+		switch (i) {
+		
+		case 10:
+			var button = Ti.UI.createButton({
+				title: 'Machine',
+				//color: '#ffffff',
+				width: '25%',
+				left: 7+(30*(tl-1))+'%',
+				top: 6+(20*tt)+'%',
+				height: '15%'
+			});
+			button.addEventListener("click", function(){ 
+				var attempt = new MR(Ti.App.code);
+				Ti.App.code = '';
+			});		
+		break;
+		case 11:
+			var button = Ti.UI.createButton({
+				title: '0',
+				//color: '#ffffff',
+				width: '25%',
+				left: 7+(30*(tl-1))+'%',
+				top: 6+(20*tt)+'%',
+				height: '15%',
+				backgroundImage: 'images/button.png',
+				backgroundFocusedImage: 'images/button-down.png',
+				backgroundSelectedImage: 'images/button-down.png'
+			});
+			button.addEventListener("click", function(){ 
+				Ti.App.code += this.title;
+			});
+		break;
+		case 12:
+			var button = Ti.UI.createButton({
+				title: 'Passport',
+				//color: '#ffffff',
+				width: '25%',
+				left: 7+(30*(tl-1))+'%',
+				top: 6+(20*tt)+'%',
+				height: '15%'
+			});
+			button.addEventListener("click", function(){ 
+				var attempt = new PassPort(Ti.App.code);
+				Ti.App.code = '';
+			});
+		break;
+		default:
+			var button = Ti.UI.createButton({
+				title: i,
+				//backgroundImage:"images/arcade-button.png",
+				width: '25%',
+				left: 7+(30*(tl-1))+'%',
+				top: 6+(20*tt)+'%',
+				height: '15%',
+				backgroundImage: 'images/button.png',
+				backgroundFocusedImage: 'images/button-down.png',
+				backgroundSelectedImage: 'images/button-down.png'
+			});	
+			button.addEventListener("click", function(){ 
+				Ti.App.code += this.title;
+			});
+		}
+
+
+		self.add(button);
+	}
+
+	
+	return self;
+}
+
+module.exports = FirstView;
---
+++ Resources/ui/common/passport.js
@@ -0,0 +1,24 @@
+function passport(code) {
+ 
+ 	var url = "http://passport.xinchejian.com:8080/lock?pin="+code;
+	 var client = Ti.Network.createHTTPClient({
+	     // function called when the response data is available
+	 onload : function(e) {
+	     Ti.API.info("Received text: " + this.responseText);
+	 	return this.responseText;
+	 },
+	 // function called when an error occurs, including a timeout
+	 onerror : function(e) {
+	     Ti.API.debug(e.error);
+	     alert('error, i think i f**ked up');
+	 },
+	 timeout : 2000  // in milliseconds
+	 });
+	 // Prepare the connection.
+	 client.open("GET", url);
+	 // Send the request.
+	 client.send(); 
+     
+};
+
+module.exports = passport;
